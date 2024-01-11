ca480de21ad5f744440896e7fd048f7444c0ffbc
Lucas Couto <lucascouto91@gmail.com>
2024-01-11 16:29:45 -0300

    feat: create GetMapping.

--- src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -1,10 +1,12 @@
 package com.mercadolivro.repository
 
+import com.mercadolivro.enums.BooksStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 import org.springframework.data.repository.CrudRepository
 
 interface BookRepository : CrudRepository<BookModel, Int> {
+    fun findByStatus(status: BooksStatus): List<BookModel>
 
 
 }
--- src/main/kotlin/com/mercadolivro/service/BookService.kt
+++ src/main/kotlin/com/mercadolivro/service/BookService.kt
@@ -1,5 +1,6 @@
 package com.mercadolivro.service
 
+import com.mercadolivro.enums.BooksStatus
 import com.mercadolivro.model.BookModel
 import com.mercadolivro.repository.BookRepository
 import org.springframework.stereotype.Service
@@ -10,6 +11,18 @@ class BookService(
 ) {
     fun create(book: BookModel) {
         bookRepository.save(book)
+    }
+
+    fun findAll(): List<BookModel> {
+        return bookRepository.findAll().toList()
+    }
+
+    fun findActives(): List<BookModel> {
+        return bookRepository.findByStatus(BooksStatus.ATIVO)
+    }
+
+    fun findById(id: Int): BookModel {
+        return bookRepository.findById(id).orElseThrow()
     }
 
 }
