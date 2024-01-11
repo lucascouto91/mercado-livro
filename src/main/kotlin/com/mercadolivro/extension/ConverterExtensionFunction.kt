6ba917ee8f3fb1c7568ba5c49a44571524f4ba2a
Lucas Couto <lucascouto91@gmail.com>
2024-01-10 23:57:06 -0300

    feat: create PostBook and save db.

--- src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
+++ src/main/kotlin/com/mercadolivro/extension/ConverterExtensionFunction.kt
@@ -1,7 +1,10 @@
 package com.mercadolivro.extension
 
+import com.mercadolivro.controller.request.PostBookRequest
 import com.mercadolivro.controller.request.PostCustomerRequest
 import com.mercadolivro.controller.request.PutCustomerRequest
+import com.mercadolivro.enums.BooksStatus
+import com.mercadolivro.model.BookModel
 import com.mercadolivro.model.CustomerModel
 
 fun PostCustomerRequest.toCustomerModel(): CustomerModel {
@@ -12,3 +15,11 @@ fun PutCustomerRequest.toCustomerModel(id: Int): CustomerModel {
     return CustomerModel(id = id,name = this.name, email = this.email)
 }
 
+fun PostBookRequest.toBookModel(customer: CustomerModel): BookModel {
+    return BookModel(
+        name = this.name,
+        price = this.price,
+        status = BooksStatus.ATVIO,
+        customer = customer
+    )
+}
---
+++ src/main/kotlin/com/mercadolivro/repository/BookRepository.kt
@@ -0,0 +1,10 @@
+package com.mercadolivro.repository
+
+import com.mercadolivro.model.BookModel
+import com.mercadolivro.model.CustomerModel
+import org.springframework.data.repository.CrudRepository
+
+interface BookRepository : CrudRepository<BookModel, Int> {
+
+
+}
