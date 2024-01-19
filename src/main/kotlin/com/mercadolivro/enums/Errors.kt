f4524419fc5220ed988db5532132a613a70062ba
Lucas Couto <lucascouto91@gmail.com>
2024-01-18 23:40:48 -0300

    feat: exception handled Spring @valid.

--- src/main/kotlin/com/mercadolivro/enums/Errors.kt
+++ src/main/kotlin/com/mercadolivro/enums/Errors.kt
@@ -2,8 +2,9 @@ package com.mercadolivro.enums
 
 enum class Errors(val code: String, val message: String) {
 
-    ML101("ML-101","Book [%s] not exists"),
-    ML102("ML-102","Cannot update book with status [%s]"),
-    ML201("ML-201","Book [%s] not exists")
+    ML001("ML-001", "Invalid Request"),
+    ML101("ML-101", "Book [%s] not exists"),
+    ML102("ML-102", "Cannot update book with status [%s]"),
+    ML201("ML-201", "Book [%s] not exists")
 
 }
