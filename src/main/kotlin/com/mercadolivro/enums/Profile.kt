7437f5a136225bb408870273b0cc7c2afe0d4921
Lucas Couto <lucascouto91@gmail.com>
2024-01-27 15:16:48 -0300

    feat: created roles table.

---
+++ src/main/kotlin/com/mercadolivro/enums/Profile.kt
@@ -0,0 +1,9 @@
+package com.mercadolivro.enums
+
+import org.springframework.context.annotation.Description
+
+enum class Profile(val description: String) {
+    ADMIN("ADMIN_ROLE"),
+    CUSTOMER("CUSTOMER)ROLE")
+
+}
