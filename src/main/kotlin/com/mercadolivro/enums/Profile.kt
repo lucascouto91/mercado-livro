4228c510d78a1bf30d96b93181e4c5c9bfcc0b42
Lucas Couto <lucascouto91@gmail.com>
2024-01-27 15:18:08 -0300

    style: removed import.

--- src/main/kotlin/com/mercadolivro/enums/Profile.kt
+++ src/main/kotlin/com/mercadolivro/enums/Profile.kt
@@ -1,7 +1,5 @@
 package com.mercadolivro.enums
 
-import org.springframework.context.annotation.Description
-
 enum class Profile(val description: String) {
     ADMIN("ADMIN_ROLE"),
     CUSTOMER("CUSTOMER)ROLE")
