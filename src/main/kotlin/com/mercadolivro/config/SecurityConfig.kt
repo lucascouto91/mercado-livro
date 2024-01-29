2a4e5bbc320a97c4f53348af1de5d21508943024
Lucas Couto <lucascouto91@gmail.com>
2024-01-28 21:57:34 -0300

    feat: added Spring Security configuration.

--- src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
+++ src/main/kotlin/com/mercadolivro/config/SecurityConfig.kt
@@ -2,10 +2,30 @@ package com.mercadolivro.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
+@EnableWebSecurity
 class SecurityConfig {
+
+    private val publicPostMatchers = arrayOf("/customer")
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        return http
+            .cors {}
+            .csrf { it.disable() }
+            .sessionManagement { session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS) }
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers(HttpMethod.POST, *publicPostMatchers).permitAll()
+                    .anyRequest().authenticated()
+            }.build()
+    }
 
     @Bean
     fun bCryptPasswordEncoder(): BCryptPasswordEncoder {
