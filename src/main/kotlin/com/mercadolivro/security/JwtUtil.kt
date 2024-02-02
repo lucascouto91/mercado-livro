783cc57188f625e9eb6f0585652075e017de942d
Lucas Couto <lucascouto91@gmail.com>
2024-02-02 02:21:12 -0300

    fix: changed methods deprecated.

--- src/main/kotlin/com/mercadolivro/security/JwtUtil.kt
+++ src/main/kotlin/com/mercadolivro/security/JwtUtil.kt
@@ -1,9 +1,11 @@
 package com.mercadolivro.security
 
 import io.jsonwebtoken.Jwts
-import io.jsonwebtoken.SignatureAlgorithm
+import io.jsonwebtoken.security.Keys
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.security.Key
 import java.util.*
 
 @Component
@@ -16,10 +18,11 @@ class JwtUtil {
     private val secret: String? = null
 
     fun generateToken(id: Int): String {
+        val key : Key = Keys.hmacShaKeyFor(secret!!.toByteArray(StandardCharsets.UTF_8))
         return Jwts.builder()
-            .setSubject(id.toString())
-            .setExpiration(Date(System.currentTimeMillis() + expiration!!))
-            .signWith(SignatureAlgorithm.HS512, secret!!.toByteArray())
+            .subject(id.toString())
+            .expiration(Date(System.currentTimeMillis() + expiration!!))
+            .signWith(key)
             .compact()
     }
 
